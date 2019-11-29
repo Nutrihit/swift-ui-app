701d7417f549d454d9f463015ee2c4a9df455b1a
Dimitri Schrul <dimitri.schrul@gmail.com>
2019-11-29 22:29:39 +0100

    Initial Commit

---
+++ nutrihit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  nutrihit
+//
+//  Created by Dimitri Schrul on 29.11.19.
+//  Copyright © 2019 Dimitri Schrul. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
